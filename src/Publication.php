817bd9678587cc75bc132aa0f36fd033baf09dee
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-16 15:20:28 +0000

    import categories from publication

--- src/Publication.php
+++ src/Publication.php
@@ -3,8 +3,11 @@ namespace razorbacks\walton\news\feed;
 
 use Crontab\Job;
 use DateTime;
+use Exception;
 
 class Publication extends Job {
+	protected $categories;
+
 	public function getNextRuntime(){
 		$date = new DateTime(date('h:i:s'));
 		$dminute = (int)$date->format('i');
@@ -17,5 +20,30 @@ class Publication extends Job {
 
 		$date->setTime((int)$date->format('h'), $jminute);
 		return $date->format('h:i A');
+	}
+
+	protected function importCategories(){
+		$command = $this->getCommand();
+
+		// break off query string
+		if (strpos($command, '?') === false) {
+			throw new Exception('No query string found.');
+		}
+		$pieces = explode('?', $command);
+		$query = $pieces[1];
+
+		// get the categories
+		if (strpos($query, 'categories') === false) {
+			throw new Exception('No categories found.');
+		}
+		parse_str($query);
+		$this->categories = $categories;
+	}
+
+	public function getCategories(){
+		if(empty($this->categories)){
+			$this->importCategories();
+		}
+		return $this->categories;
 	}
 }
