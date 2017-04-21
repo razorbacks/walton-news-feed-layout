7a78172a770d2fe8c048738f2b02e8c7ea801fcf
Jeff Puckett <jeff@jeffpuckett.com>
2017-04-21 19:39:14 +0000

    fix whitespace code style

--- src/Publication.php
+++ src/Publication.php
@@ -6,207 +6,223 @@ use DateTime;
 use Exception;
 use InvalidArgumentException;
 
-class Publication extends Job {
-	protected $categories;
-	protected $count;
-	protected $view;
-	protected $valid = true;
+class Publication extends Job
+{
+    protected $categories;
+    protected $count;
+    protected $view;
+    protected $valid = true;
 
-	public function __construct($array, $minute){
-		$this->initialize($array, $minute);
-	}
+    public function __construct($array, $minute)
+    {
+        $this->initialize($array, $minute);
+    }
 
-	public function setCommand($command){
-		// escape % for crontab
-		// https://github.com/yzalis/Crontab/issues/34
-		// http://www.ducea.com/2008/11/12/using-the-character-in-crontab-entries/
-		$return = parent::setCommand(str_replace('%', '\\\\%', $command));
-		$this->parseCommand();
-		return $return;
-	}
+    public function setCommand($command)
+    {
+        // escape % for crontab
+        // https://github.com/yzalis/Crontab/issues/34
+        // http://www.ducea.com/2008/11/12/using-the-character-in-crontab-entries/
+        $return = parent::setCommand(str_replace('%', '\\\\%', $command));
+        $this->parseCommand();
+        return $return;
+    }
 
-	public function getCommand(){
-		// unescape from in-memory
-		$return = str_replace('\\\\%', '%', parent::getCommand());
-		// unescape from crontab
-		return str_replace('\\%', '%', $return);
-	}
+    public function getCommand()
+    {
+        // unescape from in-memory
+        $return = str_replace('\\\\%', '%', parent::getCommand());
+        // unescape from crontab
+        return str_replace('\\%', '%', $return);
+    }
 
-	public function getNextRuntime(){
-		if(!$this->valid){
-			return false;
-		}
+    public function getNextRuntime()
+    {
+        if(!$this->valid){
+            return false;
+        }
 
-		$date = new DateTime(date('H:i:s'));
-		$dminute = (int)$date->format('i');
-		$jminute = (int)$this->getMinute();
+        $date = new DateTime(date('H:i:s'));
+        $dminute = (int)$date->format('i');
+        $jminute = (int)$this->getMinute();
 
-		// if minute passed, increment hour
-		if($jminute <= $dminute){
-			$date->modify("+1 hour");
-		}
+        // if minute passed, increment hour
+        if($jminute <= $dminute){
+            $date->modify("+1 hour");
+        }
 
-		$date->setTime((int)$date->format('H'), $jminute);
-		return $date->format('h:i A');
-	}
+        $date->setTime((int)$date->format('H'), $jminute);
+        return $date->format('h:i A');
+    }
 
-	public function getLastRuntime(){
-		$filename = $this->getPublicationFilename();
-		$modified = filemtime($filename);
-		if($modified === false){
-			return "File doesn't exist: $filename";
-		}
+    public function getLastRuntime()
+    {
+        $filename = $this->getPublicationFilename();
+        $modified = filemtime($filename);
+        if($modified === false){
+            return "File doesn't exist: $filename";
+        }
 
-		$datetime = new DateTime;
-		$datetime->setTimestamp($modified);
-		return $datetime->format('Y-m-d H:i - h:i A l, F d, Y');
-	}
+        $datetime = new DateTime;
+        $datetime->setTimestamp($modified);
+        return $datetime->format('Y-m-d H:i - h:i A l, F d, Y');
+    }
 
-	public function parseCommand(){
-		$pieces = explode(' ', $this->getCommand());
+    public function parseCommand()
+    {
+        $pieces = explode(' ', $this->getCommand());
 
-		if(!isset($pieces[2])){
-			return $this->valid = false;
-		}
+        if(!isset($pieces[2])){
+            return $this->valid = false;
+        }
 
-		if( !$this->parseQueryString(trim($pieces[2],"'")) ){
-			return $this->valid = false;
-		}
-	}
+        if( !$this->parseQueryString(trim($pieces[2],"'")) ){
+            return $this->valid = false;
+        }
+    }
 
-	protected function parseQueryString($query){
-		parse_str($query, $array);
+    protected function parseQueryString($query)
+    {
+        parse_str($query, $array);
 
-		// get categories, count, and view
-		foreach(array('categories', 'count', 'view') as $property){
-			if(!isset($array[$property])){
-				return false;
-			}
-			// use custom setters with variable variables from parse_str
-			$set = "set$property";
-			$this->$set($array[$property]);
-		}
+        // get categories, count, and view
+        foreach(array('categories', 'count', 'view') as $property){
+            if(!isset($array[$property])){
+                return false;
+            }
+            // use custom setters with variable variables from parse_str
+            $set = "set$property";
+            $this->$set($array[$property]);
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	public function __get($property){
-		if(!$this->valid){
-			return false;
-		}
+    public function __get($property)
+    {
+        if(!$this->valid){
+            return false;
+        }
 
-		$properties = array('valid', 'categories', 'count', 'view');
-		if(!in_array($property, $properties, true)){
-			$properties = implode(', ', $properties);
-			throw new InvalidArgumentException("Only $properties are accessible");
-		}
-		if(empty($this->$property)){
-			$this->parseCommand();
-		}
-		if(empty($this->$property)){
-			return false;
-		}
-		return $this->$property;
-	}
+        $properties = array('valid', 'categories', 'count', 'view');
+        if(!in_array($property, $properties, true)){
+            $properties = implode(', ', $properties);
+            throw new InvalidArgumentException("Only $properties are accessible");
+        }
+        if(empty($this->$property)){
+            $this->parseCommand();
+        }
+        if(empty($this->$property)){
+            return false;
+        }
+        return $this->$property;
+    }
 
-	public function setCategories($categories){
-		if(!is_array($categories)){
-			throw new InvalidArgumentException('Categories must be in array.');
-		}
-		foreach($categories as &$category){
-			$category = filter_var($category, FILTER_VALIDATE_INT);
-			if(!is_int($category)){
-				throw new InvalidArgumentException("Category must be integer.");
-			}
-			if($category < 1){
-				throw new InvalidArgumentException("Category must be positive. $category given.");
-			}
-		}
+    public function setCategories($categories)
+    {
+        if(!is_array($categories)){
+            throw new InvalidArgumentException('Categories must be in array.');
+        }
+        foreach($categories as &$category){
+            $category = filter_var($category, FILTER_VALIDATE_INT);
+            if(!is_int($category)){
+                throw new InvalidArgumentException("Category must be integer.");
+            }
+            if($category < 1){
+                throw new InvalidArgumentException("Category must be positive. $category given.");
+            }
+        }
 
-		sort($categories);
-		$this->categories = $categories;
-		return $this;
-	}
+        sort($categories);
+        $this->categories = $categories;
+        return $this;
+    }
 
-	public function setCount($count){
-		$count = filter_var($count, FILTER_VALIDATE_INT);
-		if(!is_int($count)){
-			throw new InvalidArgumentException("Count must be integer.");
-		}
-		if($count < 1){
-			throw new InvalidArgumentException("Count must be positive. $count given.");
-		}
+    public function setCount($count)
+    {
+        $count = filter_var($count, FILTER_VALIDATE_INT);
+        if(!is_int($count)){
+            throw new InvalidArgumentException("Count must be integer.");
+        }
+        if($count < 1){
+            throw new InvalidArgumentException("Count must be positive. $count given.");
+        }
 
-		$this->count = $count;
-		return $this;
-	}
+        $this->count = $count;
+        return $this;
+    }
 
-	public function setView($view){
-		if(!is_string($view)){
-			throw new InvalidArgumentException("View name must be a string.");
-		}
+    public function setView($view)
+    {
+        if(!is_string($view)){
+            throw new InvalidArgumentException("View name must be a string.");
+        }
 
-		$views = __DIR__."/../views";
-		if(!file_exists("$views/$view.twig.html")){
-			throw new InvalidArgumentException(
-				"$view does not exist."
-			);
-		}
+        $views = __DIR__."/../views";
+        if(!file_exists("$views/$view.twig.html")){
+            throw new InvalidArgumentException(
+                "$view does not exist."
+            );
+        }
 
-		$this->view = $view;
-		return $this;
-	}
+        $this->view = $view;
+        return $this;
+    }
 
-	public function getPublicationFilename(){
-		if(!$this->valid){
-			return false;
-		}
+    public function getPublicationFilename()
+    {
+        if(!$this->valid){
+            return false;
+        }
 
-		$view  = $this->__get('view');
-		$count = $this->__get('count');
-		$categories = $this->__get('categories');
-		$categories = implode('-', $categories);
+        $view  = $this->__get('view');
+        $count = $this->__get('count');
+        $categories = $this->__get('categories');
+        $categories = implode('-', $categories);
 
-		$directory  = realpath(__DIR__."/../publications");
-		return "$directory/$view.$count.$categories.html";
-	}
+        $directory  = realpath(__DIR__."/../publications");
+        return "$directory/$view.$count.$categories.html";
+    }
 
-	public function getIncludeScript(){
-		return
-			"<?php /* News Publication: ".
-			$this->getComments().
-			" */ include '".
-			$this->getPublicationFilename().
-			"'; ?>";
-	}
+    public function getIncludeScript()
+    {
+        return
+            "<?php /* News Publication: ".
+            $this->getComments().
+            " */ include '".
+            $this->getPublicationFilename().
+            "'; ?>";
+    }
 
-	protected function buildQueryString(){
-		$data = array(
-			'categories' => $this->__get('categories'),
-			'count'      => $this->__get('count'),
-			'view'       => $this->__get('view'),
-		);
-		return http_build_query($data);
-	}
+    protected function buildQueryString()
+    {
+        $data = array(
+            'categories' => $this->__get('categories'),
+            'count'      => $this->__get('count'),
+            'view'       => $this->__get('view'),
+        );
+        return http_build_query($data);
+    }
 
-	public function initialize($array, $minute){
-		$this
-			->setCategories($array['categories'])
-			->setCount($array['count'])
-			->setView($array['view'])
-			->setMinute($minute)
-			->setHour('*')
-			->setDayOfMonth('*')
-			->setMonth('*')
-			->setDayOfWeek('*')
-			->setCommand(
-				'/usr/bin/php ' .
-				realpath(__DIR__.'/../pages/getlayout.php') .
-				" '" . $this->buildQueryString() . "'" .
-				" '" . $this->getPublicationFilename() . "'" .
-				" > /dev/null 2>&1"
-			)
-			->setComments($array['comments'])
-		;
-	}
+    public function initialize($array, $minute)
+    {
+        $this
+            ->setCategories($array['categories'])
+            ->setCount($array['count'])
+            ->setView($array['view'])
+            ->setMinute($minute)
+            ->setHour('*')
+            ->setDayOfMonth('*')
+            ->setMonth('*')
+            ->setDayOfWeek('*')
+            ->setCommand(
+                '/usr/bin/php ' .
+                realpath(__DIR__.'/../pages/getlayout.php') .
+                " '" . $this->buildQueryString() . "'" .
+                " '" . $this->getPublicationFilename() . "'" .
+                " > /dev/null 2>&1"
+            )
+            ->setComments($array['comments'])
+        ;
+    }
 }
--- tests/LayoutTest.php
+++ tests/LayoutTest.php
@@ -1,24 +1,25 @@
 <?php
 use razorbacks\walton\news\Layout;
 
-class LayoutTest extends PHPUnit_Framework_TestCase {
-	public function test_creates_list_layout_with_default_images(){
-		$json     = file_get_contents(__DIR__."/json/posts-no-image.json");
-		$expected = file_get_contents(__DIR__."/html/list-no-image.html");
+class LayoutTest extends PHPUnit_Framework_TestCase
+{
+    public function test_creates_list_layout_with_default_images(){
+        $json     = file_get_contents(__DIR__."/json/posts-no-image.json");
+        $expected = file_get_contents(__DIR__."/html/list-no-image.html");
 
-		$layout = new Layout($json, array(40, 22), 4, 'list');
-		$actual = $layout->render();
+        $layout = new Layout($json, array(40, 22), 4, 'list');
+        $actual = $layout->render();
 
-		$this->assertSame((string)$layout, $actual);
-		$this->assertSame($expected, $actual);
-	}
+        $this->assertSame((string)$layout, $actual);
+        $this->assertSame($expected, $actual);
+    }
 
-	/**
-	 * @expectedException InvalidArgumentException
-	 */
-	public function test_invalidates_json(){
-		$invalid = file_get_contents(__DIR__.'/json/invalid.json');
-		$generator = new Layout($invalid, 1, 1, 'list');
-		$this->assertTrue(false);
-	}
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function test_invalidates_json(){
+        $invalid = file_get_contents(__DIR__.'/json/invalid.json');
+        $generator = new Layout($invalid, 1, 1, 'list');
+        $this->assertTrue(false);
+    }
 }
