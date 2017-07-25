d06c964de09c1e3f92cb1bb46997100d39562e43
Jeff Puckett <jeff@jeffpuckett.com>
2017-07-25 19:45:26 +0000

    cs fix ws tabs v spaces

--- src/Layout.php
+++ src/Layout.php
@@ -7,166 +7,166 @@ use Twig_Environment;
 use Twig_Loader_Filesystem;
 
 class Layout {
-	protected $news;
-	protected $categories;
-	protected $number_of_posts_to_show;
-	protected $html;
+    protected $news;
+    protected $categories;
+    protected $number_of_posts_to_show;
+    protected $html;
 
-	public function __construct($feed, $categories, $count, $view){
-		$views = __DIR__."/../views";
-		if(!file_exists("$views/$view.twig.html")){
-			throw new InvalidArgumentException(
-				"$view does not exist."
-			);
-		}
+    public function __construct($feed, $categories, $count, $view){
+        $views = __DIR__."/../views";
+        if(!file_exists("$views/$view.twig.html")){
+            throw new InvalidArgumentException(
+                "$view does not exist."
+            );
+        }
 
-		$feed = utf8_encode($feed);
-		$this->news = json_decode($feed, true);
-		if (!is_array($this->news)){
-			throw new InvalidArgumentException(
-				"JSON Error #".json_last_error().
-				". see http://php.net/manual/en/function.json-last-error.php"
-			);
-		}
-		if ( empty($this->news) ) {
-			throw new InvalidArgumentException(
-				"Feed is empty."
-			);
-		}
+        $feed = utf8_encode($feed);
+        $this->news = json_decode($feed, true);
+        if (!is_array($this->news)){
+            throw new InvalidArgumentException(
+                "JSON Error #".json_last_error().
+                ". see http://php.net/manual/en/function.json-last-error.php"
+            );
+        }
+        if ( empty($this->news) ) {
+            throw new InvalidArgumentException(
+                "Feed is empty."
+            );
+        }
 
-		if(!is_array($categories)){
-			$categories = array($categories);
-		}
+        if(!is_array($categories)){
+            $categories = array($categories);
+        }
 
-		foreach($categories as $category){
-			$category = filter_var($category, FILTER_VALIDATE_INT);
-			if(is_int($category) && $category > 0){
-				$this->categories[]= $category;
-			} else {
-				throw new InvalidArgumentException(
-					"category IDs must be a positive integer"
-				);
-			}
-		}
+        foreach($categories as $category){
+            $category = filter_var($category, FILTER_VALIDATE_INT);
+            if(is_int($category) && $category > 0){
+                $this->categories[]= $category;
+            } else {
+                throw new InvalidArgumentException(
+                    "category IDs must be a positive integer"
+                );
+            }
+        }
 
-		$count = filter_var($count, FILTER_VALIDATE_INT);
-		if(is_int($count) && $count > 0){
-			$this->number_of_posts_to_show = $count;
-		} else {
-			throw new InvalidArgumentException(
-				"number of posts to show must a positive integer"
-			);
-		}
+        $count = filter_var($count, FILTER_VALIDATE_INT);
+        if(is_int($count) && $count > 0){
+            $this->number_of_posts_to_show = $count;
+        } else {
+            throw new InvalidArgumentException(
+                "number of posts to show must a positive integer"
+            );
+        }
 
-		$twig = new Twig_Environment(
-			new Twig_Loader_Filesystem($views),
-			array('autoescape' => false)
-		);
-		$this->build($twig->loadTemplate("$view.twig.html"));
-	}
+        $twig = new Twig_Environment(
+            new Twig_Loader_Filesystem($views),
+            array('autoescape' => false)
+        );
+        $this->build($twig->loadTemplate("$view.twig.html"));
+    }
 
-	protected function build($template){
-		$featured_items = array();
-		$regular_items = array();
-		$count = 0;
-		foreach ($this->news as $item){
-			if(++$count > $this->number_of_posts_to_show){
-				break;
-			}
+    protected function build($template){
+        $featured_items = array();
+        $regular_items = array();
+        $count = 0;
+        foreach ($this->news as $item){
+            if(++$count > $this->number_of_posts_to_show){
+                break;
+            }
 
-			// canonicalize rendered item attributes
-			$item['title'] = $item['title']['rendered'];
-			$item['excerpt'] = $item['excerpt']['rendered'];
+            // canonicalize rendered item attributes
+            $item['title'] = $item['title']['rendered'];
+            $item['excerpt'] = $item['excerpt']['rendered'];
 
-			// images
-			if (!empty($item['_links']['wp:featuredmedia'][0]['href'])) {
-				$url = $item['_links']['wp:featuredmedia'][0]['href'];
-				$item['image'] = $this->fetchImageUrls($url);
-			} else {
-				$item['image'] = array();
-			}
-			$item['image'] = $this->getDefaultImages($item['image']);
+            // images
+            if (!empty($item['_links']['wp:featuredmedia'][0]['href'])) {
+                $url = $item['_links']['wp:featuredmedia'][0]['href'];
+                $item['image'] = $this->fetchImageUrls($url);
+            } else {
+                $item['image'] = array();
+            }
+            $item['image'] = $this->getDefaultImages($item['image']);
 
-			// show featured items first
-			$featured = false;
-			foreach ($item["categories"] as $category){
-				if ($category == getenv('NEWS_PUBLICATION_FEATURED_CATEGORY_ID')) {
-					$featured = true;
-					break;
-				}
-			}
-			if ($featured){
-				$featured_items []= $item;
-			} else {
-				$regular_items  []= $item;
-			}
-		}
+            // show featured items first
+            $featured = false;
+            foreach ($item["categories"] as $category){
+                if ($category == getenv('NEWS_PUBLICATION_FEATURED_CATEGORY_ID')) {
+                    $featured = true;
+                    break;
+                }
+            }
+            if ($featured){
+                $featured_items []= $item;
+            } else {
+                $regular_items  []= $item;
+            }
+        }
 
-		$items = array_merge($featured_items, $regular_items);
-		$this->html = $template->render(array('items' => $items));
-	}
+        $items = array_merge($featured_items, $regular_items);
+        $this->html = $template->render(array('items' => $items));
+    }
 
-	/**
-	 * fetch the URLs for a post's featured image and thumbnail
-	 *
-	 * @param  string $url wp-json post media endpoint
-	 * @return array  contains image URLs
-	 */
-	protected function fetchImageUrls($url)
-	{
-		$json = file_get_contents($url);
-		if ( empty($json) ) {
-			throw new Exception("Nothing returned from URL: $url");
-		}
+    /**
+     * fetch the URLs for a post's featured image and thumbnail
+     *
+     * @param  string $url wp-json post media endpoint
+     * @return array  contains image URLs
+     */
+    protected function fetchImageUrls($url)
+    {
+        $json = file_get_contents($url);
+        if ( empty($json) ) {
+            throw new Exception("Nothing returned from URL: $url");
+        }
 
-		$media = json_decode($json, $array = true);
-		if ( !is_array($media) ) {
-			throw new Exception(
-				"JSON Error #".json_last_error().
-				". see http://php.net/manual/en/function.json-last-error.php"
-			);
-		}
+        $media = json_decode($json, $array = true);
+        if ( !is_array($media) ) {
+            throw new Exception(
+                "JSON Error #".json_last_error().
+                ". see http://php.net/manual/en/function.json-last-error.php"
+            );
+        }
 
-		$array = array();
+        $array = array();
 
-		foreach ( $media['media_details']['sizes'] as $size => $image ) {
-			$array[$size] = $image['source_url'];
-		}
+        foreach ( $media['media_details']['sizes'] as $size => $image ) {
+            $array[$size] = $image['source_url'];
+        }
 
-		return $array;
-	}
+        return $array;
+    }
 
-	/**
-	 * fills any missing image sizes with defaults
-	 *
-	 * @param  array $images
-	 * @return array
-	 */
-	public function getDefaultImages($images)
-	{
-		$sizes = array(
-			'thumbnail',
-			'medium',
-			'medium_large',
-			'large',
-			'full',
-		);
+    /**
+     * fills any missing image sizes with defaults
+     *
+     * @param  array $images
+     * @return array
+     */
+    public function getDefaultImages($images)
+    {
+        $sizes = array(
+            'thumbnail',
+            'medium',
+            'medium_large',
+            'large',
+            'full',
+        );
 
-		foreach ( $sizes as $size ) {
-			if ( empty($images[$size]) ) {
-				$env = strtoupper("NEWS_PUBLICATION_DEFAULT_IMAGE_$size");
-				$images[$size] = getenv($env);
-			}
-		}
+        foreach ( $sizes as $size ) {
+            if ( empty($images[$size]) ) {
+                $env = strtoupper("NEWS_PUBLICATION_DEFAULT_IMAGE_$size");
+                $images[$size] = getenv($env);
+            }
+        }
 
-		return $images;
-	}
+        return $images;
+    }
 
-	public function render(){
-		return $this->html;
-	}
+    public function render(){
+        return $this->html;
+    }
 
-	public function __toString(){
-		return $this->render();
-	}
+    public function __toString(){
+        return $this->render();
+    }
 }
--- src/Scheduler.php
+++ src/Scheduler.php
@@ -7,121 +7,121 @@ use Exception;
 use InvalidArgumentException;
 
 class Scheduler extends Crontab {
-	protected function castJobToPublication(Job $job) {
-		$class = __NAMESPACE__ . '\Publication';
-		/**
-		* This is a beautifully ugly hack.
-		* https://gist.github.com/duaiwe/960035
-		*
-		* First, we serialize our object, which turns it into a string, allowing
-		* us to muck about with it using standard string manipulation methods.
-		*
-		* Then, we use preg_replace to change it's defined type to the class
-		* we're casting it to, and then serialize the string back into an
-		* object.
-		*/
-		$publication = unserialize(
-			preg_replace(
-				'/^O:\d+:"[^"]++"/',
-				'O:'.strlen($class).':"'.$class.'"',
-				serialize($job)
-			)
-		);
+    protected function castJobToPublication(Job $job) {
+        $class = __NAMESPACE__ . '\Publication';
+        /**
+        * This is a beautifully ugly hack.
+        * https://gist.github.com/duaiwe/960035
+        *
+        * First, we serialize our object, which turns it into a string, allowing
+        * us to muck about with it using standard string manipulation methods.
+        *
+        * Then, we use preg_replace to change it's defined type to the class
+        * we're casting it to, and then serialize the string back into an
+        * object.
+        */
+        $publication = unserialize(
+            preg_replace(
+                '/^O:\d+:"[^"]++"/',
+                'O:'.strlen($class).':"'.$class.'"',
+                serialize($job)
+            )
+        );
 
-		$publication->parseCommand();
+        $publication->parseCommand();
 
-		return $publication;
-	}
+        return $publication;
+    }
 
-	public function getPublications(){
-		$publications = array();
-		foreach($this->getJobs() as $job){
-			if($job instanceof Publication){
-				$publication = $job;
-			} else {
-				$publication = $this->castJobToPublication($job);
-			}
-			if($publication->valid){
-				$publications[$publication->getHash()] = $publication;
-			}
-		}
-		return $publications;
-	}
+    public function getPublications(){
+        $publications = array();
+        foreach($this->getJobs() as $job){
+            if($job instanceof Publication){
+                $publication = $job;
+            } else {
+                $publication = $this->castJobToPublication($job);
+            }
+            if($publication->valid){
+                $publications[$publication->getHash()] = $publication;
+            }
+        }
+        return $publications;
+    }
 
-	public function runPublication($hash){
-		$publications = $this->getPublications();
-		if(!isset($publications[$hash])){
-			throw new InvalidArgumentException("Publication does not exist.");
-		}
+    public function runPublication($hash){
+        $publications = $this->getPublications();
+        if(!isset($publications[$hash])){
+            throw new InvalidArgumentException("Publication does not exist.");
+        }
 
-		$cmd = $publications[$hash]->getCommandUnescaped();
-		exec($cmd, $output, $return);
-		if($return != 0){
-			throw new Exception('Run Publication Error: '.implode(PHP_EOL, $output));
-		}
-	}
+        $cmd = $publications[$hash]->getCommandUnescaped();
+        exec($cmd, $output, $return);
+        if($return != 0){
+            throw new Exception('Run Publication Error: '.implode(PHP_EOL, $output));
+        }
+    }
 
-	public function deletePublication($hash){
-		$publications = $this->getPublications();
-		if(!isset($publications[$hash])){
-			throw new InvalidArgumentException("Publication does not exist.");
-		}
-		$this->removeJob($publications[$hash])->write();
+    public function deletePublication($hash){
+        $publications = $this->getPublications();
+        if(!isset($publications[$hash])){
+            throw new InvalidArgumentException("Publication does not exist.");
+        }
+        $this->removeJob($publications[$hash])->write();
 
-		$backup = new Backup($this);
-		$backup->save();
-	}
+        $backup = new Backup($this);
+        $backup->save();
+    }
 
-	public function getAnOpenMinute(){
-		// minutes between 5-55  to avoid on the hour jobs
-		for($i = 5; $i < 56; $i++){
-			$open []= $i;
-		}
+    public function getAnOpenMinute(){
+        // minutes between 5-55  to avoid on the hour jobs
+        for($i = 5; $i < 56; $i++){
+            $open []= $i;
+        }
 
-		// minutes that are taken by other jobs
-		$taken = array();
-		foreach($this->getJobs() as $job){
-			$minute = filter_var($job->getMinute(), FILTER_VALIDATE_INT);
-			if(is_int($minute)){
-				$taken []= $minute;
-			}
-		}
+        // minutes that are taken by other jobs
+        $taken = array();
+        foreach($this->getJobs() as $job){
+            $minute = filter_var($job->getMinute(), FILTER_VALIDATE_INT);
+            if(is_int($minute)){
+                $taken []= $minute;
+            }
+        }
 
-		$available = array_diff($open, $taken);
-		if(empty($available)){
-			return $open[array_rand($open)];
-		}
-		return $available[array_rand($available)];
-	}
+        $available = array_diff($open, $taken);
+        if(empty($available)){
+            return $open[array_rand($open)];
+        }
+        return $available[array_rand($available)];
+    }
 
-	public function createPublication($array, $execute = true){
-		$minute = $this->getAnOpenMinute();
-		$publication = new Publication($array, $minute);
+    public function createPublication($array, $execute = true){
+        $minute = $this->getAnOpenMinute();
+        $publication = new Publication($array, $minute);
 
-		// un-escape in-memory command %
-		// https://github.com/yzalis/Crontab/issues/34
-		$command = str_replace('\\%', '%', $publication->getCommand());
-		if ($execute) {
-			exec($command, $output, $return);
-			if($return != 0){
-				throw new Exception('Error: '.implode(PHP_EOL, $output));
-			}
-		}
+        // un-escape in-memory command %
+        // https://github.com/yzalis/Crontab/issues/34
+        $command = str_replace('\\%', '%', $publication->getCommand());
+        if ($execute) {
+            exec($command, $output, $return);
+            if($return != 0){
+                throw new Exception('Error: '.implode(PHP_EOL, $output));
+            }
+        }
 
-		$this->addJob($publication)->write();
+        $this->addJob($publication)->write();
 
-		$backup = new Backup($this);
-		$backup->save();
-	}
+        $backup = new Backup($this);
+        $backup->save();
+    }
 
-	public function backup()
-	{
-		$temp = new Crontab($parseExistingCrontab = false);
+    public function backup()
+    {
+        $temp = new Crontab($parseExistingCrontab = false);
 
-		foreach ($this->getPublications() as $publication) {
-			$temp->addJob($publication);
-		}
+        foreach ($this->getPublications() as $publication) {
+            $temp->addJob($publication);
+        }
 
-		return $temp->render();
-	}
+        return $temp->render();
+    }
 }
