3682f82992a8b2dc746cc8257506e8acb77c2af7
Jeff Puckett <jeff@jeffpuckett.com>
2018-01-02 17:10:34 +0000

    fix user agent string

--- pages/getlayout.php
+++ pages/getlayout.php
@@ -28,7 +28,17 @@ if(!isset($_GET['categories'],$_GET['count'],$_GET['view'])){
 		'per_page' => $_GET['count'],
 	);
 	$query = http_build_query($query);
-	$feed = file_get_contents("$endpoint?$query");
+
+	// new wordpress server rejects missing user agent string
+	$opts = [
+	    "http" => [
+	        "method" => "GET",
+	        "header" => "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0\r\n"
+	    ]
+	];
+	$context = stream_context_create($opts);
+
+	$feed = file_get_contents("$endpoint?$query", false, $context);
 
 	$layout = new Layout($feed, $_GET['categories'],$_GET['count'],$_GET['view']);
 
